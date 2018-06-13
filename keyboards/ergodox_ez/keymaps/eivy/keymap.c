df90a8ccce7e502f6af518781064c0c00566155f
Eivy <modern.times.rock.and.roll+git@gmail.com>
2018-06-13 09:39:16 +0900

    Add mouse wheel button

--- keyboards/ergodox_ez/keymaps/eivy/keymap.c
+++ keyboards/ergodox_ez/keymaps/eivy/keymap.c
@@ -167,11 +167,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
-       KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-       KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MPLY, KC_TRNS,
-                 KC_ZKHK, KC_BTN1, KC_BTN2, KC_NO,   KC_NO,   KC_TRNS,
-       KC_TRNS,  KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_TRNS,
-                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_NO,   KC_NO,         KC_NO,       KC_NO,   KC_NO,   KC_TRNS,
+       KC_TRNS,  KC_NO,   KC_MS_WH_DOWN, KC_MS_WH_UP, KC_NO,   KC_MPLY, KC_TRNS,
+                 KC_ZKHK, KC_BTN1,       KC_BTN2,     KC_NO,   KC_NO,   KC_TRNS,
+       KC_TRNS,  KC_NO,   KC_MUTE,       KC_VOLD,     KC_VOLU, KC_NO,   KC_TRNS,
+                          KC_TRNS,       KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
